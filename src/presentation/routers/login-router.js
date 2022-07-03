62cff8efe3b08656347bd538efa5140e385796be
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-03 17:16:02 -0300

    refactor: simplify code

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -1,10 +1,10 @@
+const { InvalidParamError } = require('../errors')
+const { HttpHelper } = require('../helpers')
+
 module.exports = class LoginRouter {
   async handle (httpRequest) {
     const { email, password } = httpRequest.body
-    if (!email || !password) {
-      return {
-        statusCode: 400
-      }
-    }
+    if (!email) return HttpHelper.badRequest(new InvalidParamError('email'))
+    if (!password) return HttpHelper.badRequest(new InvalidParamError('password'))
   }
 }
