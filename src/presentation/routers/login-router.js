2c6cc55d107a0de3fbb47e096d9af34a73ea003f
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-03 20:07:37 -0300

    feat: ensure LoginRouter returns 400 if validation returns an error

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -2,13 +2,19 @@ const { MissingParamError } = require('../errors')
 const { HttpHelper } = require('../helpers')
 
 module.exports = class LoginRouter {
-  constructor (authUseCase) {
+  constructor ({ authUseCase, validation } = {}) {
     this.authUseCase = authUseCase
+    this.validation = validation
   }
 
   async handle (httpRequest) {
     try {
       const { email, password } = httpRequest.body
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return HttpHelper.badRequest(error)
+      }
+
       if (!email) return HttpHelper.badRequest(new MissingParamError('email'))
       if (!password) return HttpHelper.badRequest(new MissingParamError('password'))
       const accessToken = this.authUseCase.auth(email, password)
