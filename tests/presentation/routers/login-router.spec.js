5d59b7b1243cb2cc8849dff0b1b66cc830101aca
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-03 16:50:31 -0300

    test: ensure LoginRouter returns 400 if no password is provided

--- tests/presentation/routers/login-router.spec.js
+++ tests/presentation/routers/login-router.spec.js
@@ -12,6 +12,18 @@ describe('Login Router', () => {
     const { sut } = makeSut()
     const httpRequest = {
       body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if no password is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com'
       }
     }
     const httpResponse = await sut.handle(httpRequest)
