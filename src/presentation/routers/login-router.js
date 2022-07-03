230d27c4ffd3abfe2f60bff8f347e4bb36f350cf
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-03 17:52:07 -0300

    feat: ensure LoginRouter calls AuthUseCase with correct params

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -2,11 +2,16 @@ const { MissingParamError } = require('../errors')
 const { HttpHelper } = require('../helpers')
 
 module.exports = class LoginRouter {
+  constructor (authUseCase) {
+    this.authUseCase = authUseCase
+  }
+
   async handle (httpRequest) {
     try {
       const { email, password } = httpRequest.body
       if (!email) return HttpHelper.badRequest(new MissingParamError('email'))
       if (!password) return HttpHelper.badRequest(new MissingParamError('password'))
+      this.authUseCase.auth(email, password)
     } catch (error) {
       console.error(error)
       return HttpHelper.serverError(error)
