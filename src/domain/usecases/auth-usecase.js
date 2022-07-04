2aac9b1e5245ba9ae2377f0015c0762d5692dae1
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-04 00:39:29 -0300

    feat: ensure AuthUseCase calls Encrypter with correct values

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -1,16 +1,19 @@
 const { MissingParamError, InvalidParamError } = require('../../presentation/errors')
 
 module.exports = class AuthUseCase {
-  constructor (loadUserByEmailRepository) {
+  constructor ({ loadUserByEmailRepository, encrypter } = {}) {
     this.loadUserByEmailRepository = loadUserByEmailRepository
+    this.encrypter = encrypter
   }
 
   async auth (email, password) {
     if (!this.loadUserByEmailRepository) throw new MissingParamError('LoadUserByEmailRepository')
+    if (!this.encrypter) throw new MissingParamError('Encrypter')
     if (!this.loadUserByEmailRepository.load) throw new InvalidParamError('LoadUserByEmailRepository')
     if (!email) throw new MissingParamError('email')
     if (!password) throw new MissingParamError('password')
     const user = await this.loadUserByEmailRepository.load(email)
     if (!user) return null
+    this.encrypter.compare(password, user.password)
   }
 }
