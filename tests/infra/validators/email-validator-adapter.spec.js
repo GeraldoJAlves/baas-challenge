802c89a13ca0e9b556a1c685d73a01eccfe76fb4
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-03 23:24:13 -0300

    test: ensure EmailValidatorAdapter returns false if Validator returns false

--- tests/infra/validators/email-validator-adapter.spec.js
+++ tests/infra/validators/email-validator-adapter.spec.js
@@ -1,3 +1,4 @@
+const validator = require('validator')
 const { EmailValidatorAdapter } = require('../../../src/infra/validators')
 
 const makeSut = () => {
@@ -8,7 +9,20 @@ const makeSut = () => {
 describe('Email Validator Adapter', () => {
   test('Should call Validator with correct param', () => {
     const { sut } = makeSut()
-    const isValid = sut.isValid('any_email@email.com')
+    sut.isValid('any_email@email.com')
+    expect(validator.email).toBe('any_email@email.com')
+  })
+
+  test('Should return true if Validator returns true', () => {
+    const { sut } = makeSut()
+    const isValid = sut.isValid('valid_email@email.com')
     expect(isValid).toBeTruthy()
+  })
+
+  test('Should return false if Validator returns false', () => {
+    const { sut } = makeSut()
+    validator.isEmailValid = false
+    const isValid = sut.isValid('invalid_email@email.com')
+    expect(isValid).toBeFalsy()
   })
 })
