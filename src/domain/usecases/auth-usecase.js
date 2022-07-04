6d694ab9b7195402ea03c69f62dccff43a67e3da
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-03 23:59:27 -0300

    feat: ensure AuthUseCase calls LoadUserByEmailRepository with correct param

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -1,8 +1,13 @@
 const { MissingParamError } = require('../../presentation/errors')
 
 module.exports = class AuthUseCase {
+  constructor (loadUserByEmailRepository) {
+    this.loadUserByEmailRepository = loadUserByEmailRepository
+  }
+
   async auth (email, password) {
     if (!email) throw new MissingParamError('email')
     if (!password) throw new MissingParamError('password')
+    await this.loadUserByEmailRepository.load(email)
   }
 }
