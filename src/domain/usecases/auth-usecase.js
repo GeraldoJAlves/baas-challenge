20b0dede571cc299c666b315ca38e65318556a50
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-04 01:03:15 -0300

    feat: ensure AuthUseCase calls TokenGenerator with correct userId

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -1,19 +1,23 @@
 const { MissingParamError, InvalidParamError } = require('../../presentation/errors')
 
 module.exports = class AuthUseCase {
-  constructor ({ loadUserByEmailRepository, encrypter } = {}) {
+  constructor ({ loadUserByEmailRepository, encrypter, tokenGenerator } = {}) {
     this.loadUserByEmailRepository = loadUserByEmailRepository
     this.encrypter = encrypter
+    this.tokenGenerator = tokenGenerator
   }
 
   async auth (email, password) {
     if (!this.loadUserByEmailRepository) throw new MissingParamError('LoadUserByEmailRepository')
     if (!this.encrypter) throw new MissingParamError('Encrypter')
+    if (!this.tokenGenerator) throw new MissingParamError('TokenGenerator')
     if (!this.loadUserByEmailRepository.load) throw new InvalidParamError('LoadUserByEmailRepository')
     if (!email) throw new MissingParamError('email')
     if (!password) throw new MissingParamError('password')
     const user = await this.loadUserByEmailRepository.load(email)
     if (!user) return null
-    this.encrypter.compare(password, user.password)
+    const isValid = this.encrypter.compare(password, user.password)
+    if (!isValid) return null
+    return await this.tokenGenerator.generate(user.id)
   }
 }
