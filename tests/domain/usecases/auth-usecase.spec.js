58ad63e40cdadaa16a168aaf007eae7b2e264756
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-03 23:47:04 -0300

    test: ensure AuthUseCase throws if no password is provided

--- tests/domain/usecases/auth-usecase.spec.js
+++ tests/domain/usecases/auth-usecase.spec.js
@@ -1,4 +1,5 @@
 const { AuthUseCase } = require('../../../src/domain/usecases')
+const { MissingParamError } = require('../../../src/presentation/errors')
 
 const makeSut = () => {
   const sut = new AuthUseCase()
@@ -9,6 +10,12 @@ describe('Auth UseCase', () => {
   test('Should throw if no email is provided', () => {
     const { sut } = makeSut()
     const promise = sut.auth()
-    expect(promise).rejects.toThrow()
+    expect(promise).rejects.toThrow(new MissingParamError('email'))
+  })
+
+  test('Should throw if no password is provided', () => {
+    const { sut } = makeSut()
+    const promise = sut.auth('any_email@email.com')
+    expect(promise).rejects.toThrow(new MissingParamError('password'))
   })
 })
