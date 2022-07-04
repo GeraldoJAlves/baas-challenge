9b05ceadb7d7b1aae3efe596d14ecb21fee7339e
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-04 02:04:09 -0300

    feat: ensure AuthUseCase calls UpdateAccessTokenRepository with correct values

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -1,24 +1,25 @@
-const { MissingParamError, InvalidParamError } = require('../../presentation/errors')
+const { MissingParamError } = require('../../presentation/errors')
 
 module.exports = class AuthUseCase {
-  constructor ({ loadUserByEmailRepository, encrypter, tokenGenerator } = {}) {
+  constructor ({
+    loadUserByEmailRepository,
+    encrypter,
+    tokenGenerator,
+    updateAccessTokenRepository
+  } = {}) {
     this.loadUserByEmailRepository = loadUserByEmailRepository
     this.encrypter = encrypter
     this.tokenGenerator = tokenGenerator
+    this.updateAccessTokenRepository = updateAccessTokenRepository
   }
 
   async auth (email, password) {
-    if (!this.loadUserByEmailRepository) throw new MissingParamError('LoadUserByEmailRepository')
-    if (!this.encrypter) throw new MissingParamError('Encrypter')
-    if (!this.tokenGenerator) throw new MissingParamError('TokenGenerator')
-    if (!this.loadUserByEmailRepository.load) throw new InvalidParamError('LoadUserByEmailRepository')
-    if (!this.encrypter.compare) throw new InvalidParamError('Encrypter')
-    if (!this.tokenGenerator.generate) throw new InvalidParamError('TokenGenerator')
-
     if (!email) throw new MissingParamError('email')
     if (!password) throw new MissingParamError('password')
     const user = await this.loadUserByEmailRepository.load(email)
     if (!user || !this.encrypter.compare(password, user.password)) return null
-    return await this.tokenGenerator.generate(user.id)
+    const accessToken = await this.tokenGenerator.generate(user.id)
+    await this.updateAccessTokenRepository.update(user.id, accessToken)
+    return accessToken
   }
 }
