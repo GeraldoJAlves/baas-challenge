e53d27d8f3736969886721023d401df7fe445172
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-05 03:19:56 -0300

    chore: jest mongo config

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,9 +1,12 @@
 module.exports = {
   mongodbMemoryServerOptions: {
     binary: {
+      version: '4.0.3',
       skipMD5: true
     },
-    autoStart: false,
-    instance: {}
+    instance: {
+      dbName: 'jest'
+    },
+    autoStart: false
   }
 }
