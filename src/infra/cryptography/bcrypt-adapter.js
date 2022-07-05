f1d2763159a947b4cfaa23aea91270d302464c2b
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-05 00:53:09 -0300

    feat: ensure BcryptAdapter throws if invalid params are provided

--- src/infra/cryptography/bcrypt-adapter.js
+++ src/infra/cryptography/bcrypt-adapter.js
@@ -1,7 +1,10 @@
 const bcrypt = require('bcrypt')
+const { MissingParamError } = require('../../presentation/errors')
 
 module.exports = class BcryptAdapater {
   async compare (plaintext, digest) {
+    if (!plaintext) throw new MissingParamError('plaintext')
+    if (!digest) throw new MissingParamError('digest')
     await bcrypt.compare(plaintext, digest)
   }
 }
