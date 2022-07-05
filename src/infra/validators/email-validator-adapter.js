d8d6956362cbbd5c8901adb635f358226d322765
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-05 01:17:17 -0300

    feat: ensure EmailValidatorAdapter throws if no email is provided

--- src/infra/validators/email-validator-adapter.js
+++ src/infra/validators/email-validator-adapter.js
@@ -1,7 +1,9 @@
 const validator = require('validator')
+const { MissingParamError } = require('../../presentation/errors')
 
 module.exports = class EmailValidatorAdapter {
   isValid (email) {
+    if (!email) throw new MissingParamError('email')
     return validator.isEmail(email)
   }
 }
