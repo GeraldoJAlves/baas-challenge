48265641c74c2773eadd7d6d70cf54532d2529bb
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-08 04:48:01 -0300

    chore: ignore files coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   coverageProvider: 'v8',
-  collectCoverageFrom: ['**/src/**/*.js'],
+  collectCoverageFrom: [
+    '**/src/**/*.js',
+    '!**/src/main/server.js',
+    '!**/src/main/config/env.js'
+  ],
   preset: '@shelf/jest-mongodb'
 }
