194b22702321e23054515e30032e5d8ef3b6ad36
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-09 02:26:28 -0300

    feat: ensure ValidationComposite returns an error if any validation fails

---
+++ src/validation/validators/validation-composite.js
@@ -0,0 +1,14 @@
+module.exports = class ValidationComposite {
+  constructor (validations) {
+    this.validations = validations
+  }
+
+  validate (input) {
+    for (const validation of this.validations) {
+      const error = validation.validate(input)
+      if (error) {
+        return error
+      }
+    }
+  }
+}
