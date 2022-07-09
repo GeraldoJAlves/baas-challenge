8df079019ac3f641dd5efe183a2325007405d1b2
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-09 16:45:41 -0300

    feat: ensure Signup Controller calls authUseCase with correct values

--- src/presentation/controllers/signup-controller.js
+++ src/presentation/controllers/signup-controller.js
@@ -2,9 +2,10 @@ const EmailInUseError = require('../errors/email-in-use-error')
 const { HttpHelper } = require('../helpers')
 
 module.exports = class SignupController {
-  constructor ({ addAccountUseCase, validation } = {}) {
+  constructor ({ addAccountUseCase, validation, authUseCase } = {}) {
     this.addAccountUseCase = addAccountUseCase
     this.validation = validation
+    this.authUseCase = authUseCase
   }
 
   async handle (httpRequest) {
@@ -14,6 +15,11 @@ module.exports = class SignupController {
       if (error) return HttpHelper.badRequest(error)
       const isValid = await this.addAccountUseCase.add({ name, email, password })
       if (!isValid) return HttpHelper.forbidden(new EmailInUseError())
+      const accessToken = await this.authUseCase.auth(email, password)
+      return HttpHelper.ok({
+        accessToken,
+        name
+      })
     } catch (error) {
       console.error(error)
       return HttpHelper.serverError(error)
