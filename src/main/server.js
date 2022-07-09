3c381010e84e8b37e630bf26dcca65c48c6294a6
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-09 03:00:34 -0300

    fix: setup app

--- src/main/server.js
+++ src/main/server.js
@@ -3,7 +3,7 @@ const env = require('./config/env')
 
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
-    const { setupApp } = await require('./config/app')
+    const setupApp = await require('./config/app')
     const app = await setupApp()
     app.listen(env.port, () => console.log('Server running'))
   })
