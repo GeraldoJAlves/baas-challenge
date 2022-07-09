b0358fdb194a20e59cc1822c931ecb0a409f972e
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-09 18:52:54 -0300

    feat: ensure Bcrypt Adapter hash method calls Bcrypt with correct value

--- src/infra/cryptography/bcrypt-adapter.js
+++ src/infra/cryptography/bcrypt-adapter.js
@@ -2,6 +2,14 @@ const bcrypt = require('bcrypt')
 const { MissingParamError } = require('../../presentation/errors')
 
 module.exports = class BcryptAdapater {
+  constructor (salt) {
+    this.salt = salt
+  }
+
+  async hash (plaintext) {
+    return bcrypt.hash(plaintext, this.salt)
+  }
+
   async compare (plaintext, digest) {
     if (!plaintext) throw new MissingParamError('plaintext')
     if (!digest) throw new MissingParamError('digest')
