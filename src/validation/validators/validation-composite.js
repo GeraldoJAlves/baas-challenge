a6ac238469855b6ae978c2a8e83e815f52137042
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-09 02:46:27 -0300

    feat: ensure ValidationComposite throws if no validations are provided

--- src/validation/validators/validation-composite.js
+++ src/validation/validators/validation-composite.js
@@ -1,9 +1,12 @@
+const { MissingParamError } = require('../../presentation/errors')
+
 module.exports = class ValidationComposite {
   constructor (validations) {
     this.validations = validations
   }
 
   validate (input) {
+    if (!this.validations || this.validations.length === 0) throw new MissingParamError('validations')
     for (const validation of this.validations) {
       const error = validation.validate(input)
       if (error) {
