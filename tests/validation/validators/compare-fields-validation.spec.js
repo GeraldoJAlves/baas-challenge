67bb26cce4c573ddaa66f5e8710f9476d6190a64
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-09 19:29:04 -0300

    test: ensure Compare Fields Validation return an error if input does not have fields

--- tests/validation/validators/compare-fields-validation.spec.js
+++ tests/validation/validators/compare-fields-validation.spec.js
@@ -1,4 +1,4 @@
-const { InvalidParamError } = require('../../../src/presentation/errors')
+const { InvalidParamError, MissingParamError } = require('../../../src/presentation/errors')
 const { CompareFieldsValidation } = require('../../../src/validation/validators')
 
 const makeSut = (field, fieldToCompare) => {
@@ -15,6 +15,22 @@ describe('Compare Fields Validation', () => {
       field3: 'ab'
     })
     expect(error).toEqual(new InvalidParamError('field2'))
+  })
+
+  test('Should return an error if input does not have fieldName', () => {
+    const { sut } = makeSut('field1', 'field2')
+    const error = sut.validate({
+      field2: 'ac'
+    })
+    expect(error).toEqual(new MissingParamError('field1'))
+  })
+
+  test('Should return an error if input does not have fieldToCompareName', () => {
+    const { sut } = makeSut('field1', 'field2')
+    const error = sut.validate({
+      field1: 'ab'
+    })
+    expect(error).toEqual(new MissingParamError('field2'))
   })
 
   test('Should return fasly if input have equal fields', () => {
