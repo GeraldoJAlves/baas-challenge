e3f06fba66ba6dd3818ea588c723f0036d8b8cf3
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-09 02:57:36 -0300

    chore: index validators

--- src/validation/validators/index.js
+++ src/validation/validators/index.js
@@ -1,7 +1,9 @@
 const EmailValidation = require('./email-validation')
 const RequiredField = require('./required-field')
+const ValidationComposite = require('./validation-composite')
 
 module.exports = {
   EmailValidation,
-  RequiredField
+  RequiredField,
+  ValidationComposite
 }
