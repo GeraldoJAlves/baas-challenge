595daf85d3a2472de1f84320892d1fa7e64de07f
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-09 19:58:36 -0300

    fix: remove await bcrypt

--- src/infra/cryptography/bcrypt-adapter.js
+++ src/infra/cryptography/bcrypt-adapter.js
@@ -15,6 +15,6 @@ module.exports = class BcryptAdapater {
   async compare (plaintext, digest) {
     if (!plaintext) throw new MissingParamError('plaintext')
     if (!digest) throw new MissingParamError('digest')
-    return await bcrypt.compare(plaintext, digest)
+    return bcrypt.compare(plaintext, digest)
   }
 }
