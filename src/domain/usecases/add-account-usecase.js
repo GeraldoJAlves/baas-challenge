5d5ad1a3d29785a4a224a3b59d7885c23c5cd542
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-09 17:32:48 -0300

    feat: ensure Add Account Usecase throw if no valid params are provided

---
+++ src/domain/usecases/add-account-usecase.js
@@ -0,0 +1,13 @@
+const { MissingParamError } = require('../../presentation/errors')
+
+module.exports = class AddAccountUseCase {
+  constructor ({ checkAccountByEmailRepository }) {
+    this.checkAccountByEmailRepository = checkAccountByEmailRepository
+  }
+
+  async add ({ email, name, password } = {}) {
+    if (!email) throw new MissingParamError('email')
+    if (!name) throw new MissingParamError('name')
+    if (!password) throw new MissingParamError('password')
+  }
+}
--- src/domain/usecases/index.js
+++ src/domain/usecases/index.js
@@ -1,5 +1,7 @@
 const AuthUseCase = require('./auth-usecase')
+const AddAccountUseCase = require('./add-account-usecase')
 
 module.exports = {
-  AuthUseCase
+  AuthUseCase,
+  AddAccountUseCase
 }
