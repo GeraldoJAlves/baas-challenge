cfb51b318c6ac48066fd8bdbc165c71175613d0b
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-09 17:56:17 -0300

    test: ensure Add Account Usecase calls hasher with correct password

--- tests/domain/usecases/add-account-usecase.spec.js
+++ tests/domain/usecases/add-account-usecase.spec.js
@@ -2,12 +2,15 @@ const { AddAccountUseCase } = require('../../../src/domain/usecases')
 
 const makeSut = () => {
   const checkAccountByEmailRepositorySpy = makeCheckAccountByEmailRepositorySpy()
+  const hasherSpy = makeHasherSpy()
   const sut = new AddAccountUseCase({
-    checkAccountByEmailRepository: checkAccountByEmailRepositorySpy
+    checkAccountByEmailRepository: checkAccountByEmailRepositorySpy,
+    hasher: hasherSpy
   })
   return {
     sut,
-    checkAccountByEmailRepositorySpy
+    checkAccountByEmailRepositorySpy,
+    hasherSpy
   }
 }
 
@@ -19,6 +22,17 @@ const makeCheckAccountByEmailRepositorySpy = () => {
     }
   }
   return new CheckAccountByEmailRepository()
+}
+
+const makeHasherSpy = () => {
+  class Hasher {
+    encryptedHash = 'encrypted_hash'
+    async hash (password) {
+      this.password = password
+      return this.encryptedHash
+    }
+  }
+  return new Hasher()
 }
 
 describe('Add Account Usecase', () => {
@@ -88,5 +102,15 @@ describe('Add Account Usecase', () => {
       password: 'any_password'
     })
     expect(isValid).toBeFalsy()
+  })
+
+  test('Should call hasher with correct password', async () => {
+    const { sut, hasherSpy } = makeSut()
+    await sut.add({
+      name: 'any_name',
+      email: 'any_email@email.com',
+      password: 'any_password'
+    })
+    expect(hasherSpy.password).toBe('any_password')
   })
 })
