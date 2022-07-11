0e221133364573304d6b6d969beb9d65ea419d0c
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-10 23:22:35 -0300

    feat: ensure Account Details Controller call updateAccountDetailsUseCase with correct values

--- src/presentation/controllers/account-details-controller.js
+++ src/presentation/controllers/account-details-controller.js
@@ -1,9 +1,34 @@
 module.exports = class AccountDetailsController {
-  constructor ({ validation }) {
+  constructor ({ validation, updateAccountDetailsUseCase }) {
     this.validation = validation
+    this.updateAccountDetailsUseCase = updateAccountDetailsUseCase
   }
 
   async handle (httpRequest) {
     this.validation.validate(httpRequest.body)
+    const {
+      fullName,
+      birthDate,
+      fatherName,
+      motherName,
+      rg,
+      cpf,
+      address,
+      city,
+      state,
+      cep
+    } = httpRequest.body
+    this.updateAccountDetailsUseCase.updateAccountDetails({
+      fullName,
+      birthDate,
+      fatherName,
+      motherName,
+      rg,
+      cpf,
+      address,
+      city,
+      state,
+      cep
+    })
   }
 }
