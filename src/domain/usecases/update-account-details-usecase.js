bdb6caa1e250be9038c1573ed6be072db8d2ac66
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-14 23:30:24 -0300

    feat: ensure Udpate Account Details Usecase throws if no data is provided

--- src/domain/usecases/update-account-details-usecase.js
+++ src/domain/usecases/update-account-details-usecase.js
@@ -1,9 +1,12 @@
+const { MissingParamError } = require('../../presentation/errors')
+
 module.exports = class UpdateAccountDetailsUseCase {
   constructor ({ updateAccountDetailsRepository }) {
     this.updateAccountDetailsRepository = updateAccountDetailsRepository
   }
 
-  async update (accountId, { fullName, birthDate, fatherName, motherName, rg, cpf, address, city, state, cep }) {
+  async update (accountId, { fullName, birthDate, fatherName, motherName, rg, cpf, address, city, state, cep } = {}) {
+    if (!accountId) throw new MissingParamError('accountId')
     await this.updateAccountDetailsRepository
       .updateAccountDetails(accountId, { fullName, birthDate, fatherName, motherName, rg, cpf, address, city, state, cep })
   }
