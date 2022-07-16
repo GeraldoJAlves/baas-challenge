8681f57f04ff9afb98a1b13e00d3392c3802cded
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-16 02:58:16 -0300

    test: ensure Load Account By Token Usecase calls loadAccountByTokenRepository with correct values

---
+++ tests/domain/usecases/load-account-by-token-usecase.spec.js
@@ -0,0 +1,36 @@
+const { LoadAccountByTokenUseCase } = require('../../../src/domain/usecases')
+
+const makeSut = () => {
+  const loadAccountByTokenRepositorySpy = makeLoadAccountByTokenRepositorySpy()
+  const sut = new LoadAccountByTokenUseCase({
+    loadAccountByTokenRepository: loadAccountByTokenRepositorySpy
+  })
+  return {
+    sut,
+    loadAccountByTokenRepositorySpy
+  }
+}
+
+const makeLoadAccountByTokenRepositorySpy = () => {
+  class LoadAccountByTokenRepository {
+    account = {
+      id: 'any_id'
+    }
+
+    async loadByToken (accessToken, role) {
+      this.accessToken = accessToken
+      this.role = role
+      return this.account
+    }
+  }
+  return new LoadAccountByTokenRepository()
+}
+
+describe('Load Account By Token Usecase', () => {
+  test('Shoul call loadAccountByTokenRepository with correct values', async () => {
+    const { sut, loadAccountByTokenRepositorySpy } = makeSut()
+    await sut.load('any_token', 'any_role')
+    expect(loadAccountByTokenRepositorySpy.accessToken).toBe('any_token')
+    expect(loadAccountByTokenRepositorySpy.role).toBe('any_role')
+  })
+})
