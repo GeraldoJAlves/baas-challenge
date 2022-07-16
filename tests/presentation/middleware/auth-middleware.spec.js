8e32d62c97912ecf16c3bf510c02d6cabaa6a590
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-16 02:31:42 -0300

    test: ensure Auth Middleware calls loadAccountByToken with correct token

---
+++ tests/presentation/middleware/auth-middleware.spec.js
@@ -0,0 +1,47 @@
+const { AccessDeniedError } = require('../../../src/presentation/errors')
+const { AuthMiddleware } = require('../../../src/presentation/middlewares')
+const HttpHelper = require('../../../src/presentation/helpers/http-helper')
+
+const makeSut = (role = 'user') => {
+  const loadAccountByTokenSpy = makeLoadAccountByTokenSpy()
+  const sut = new AuthMiddleware({
+    loadAccountByToken: loadAccountByTokenSpy,
+    role
+  })
+  return {
+    sut,
+    loadAccountByTokenSpy
+  }
+}
+
+const makeLoadAccountByTokenSpy = () => {
+  class LoadAccountByToken {
+    async load (accessToken, role) {
+      this.accessToken = accessToken
+      this.role = role
+      return this.account
+    }
+  }
+
+  return new LoadAccountByToken()
+}
+
+const makeRequest = () => ({
+  accessToken: 'any_token'
+})
+
+describe('Auth Middleware', () => {
+  test('Should call loadAccountByToken with correct token', async () => {
+    const { sut, loadAccountByTokenSpy } = makeSut()
+    const request = makeRequest()
+    await sut.handle(request)
+    expect(loadAccountByTokenSpy.accessToken).toBe(request.accessToken)
+    expect(loadAccountByTokenSpy.role).toBe('user')
+  })
+
+  test('Should return 403 if no token is provided', async () => {
+    const { sut } = makeSut()
+    const response = await sut.handle({})
+    expect(response).toEqual(HttpHelper.forbidden(new AccessDeniedError()))
+  })
+})
