2bd391ab3ff881c14864f100102b736703c0f1ba
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-16 04:43:31 -0300

    refactor: rename account details controller factory

--- src/main/factories/index.js
+++ src/main/factories/index.js
@@ -1,11 +1,11 @@
 const makeSignupController = require('./controllers/signup-controller-factory')
 const makeLoginController = require('./controllers/login-controller-factory')
-const makeAccountDetails = require('./controllers/account-details-controller-factory')
+const makeAccountDetailsController = require('./controllers/account-details-controller-factory')
 const makeAuthMiddleware = require('./middlewares/auth-middleware-factory')
 
 module.exports = {
   makeLoginController,
   makeSignupController,
   makeAuthMiddleware,
-  makeAccountDetails
+  makeAccountDetailsController
 }
--- src/main/routes/account-routes.js
+++ src/main/routes/account-routes.js
@@ -1,7 +1,7 @@
 const { expressRouteAdapter } = require('../adapters')
-const { makeAccountDetails } = require('../factories')
+const { makeAccountDetailsController } = require('../factories')
 const { userAuth } = require('../midlewares')
 
 module.exports = (router) => {
-  router.post('/account-details', userAuth, expressRouteAdapter(makeAccountDetails()))
+  router.post('/account-details', userAuth, expressRouteAdapter(makeAccountDetailsController()))
 }
