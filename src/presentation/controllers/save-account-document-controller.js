eed668b45303195fc589444f35a386e5d567621f
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-17 20:32:06 -0300

    feat: ensure SaveAccountDocumentCtrl returns 500 if uploadAccountDocumentUseCase throws

--- src/presentation/controllers/save-account-document-controller.js
+++ src/presentation/controllers/save-account-document-controller.js
@@ -1,10 +1,16 @@
+const { HttpHelper } = require('../helpers')
+
 module.exports = class SaveAccountDocumentController {
   constructor ({ uploadAccountDocumentUseCase } = {}) {
     this.uploadAccountDocumentUseCase = uploadAccountDocumentUseCase
   }
 
   async handle (request) {
-    const { document } = request
-    await this.uploadAccountDocumentUseCase.upload(document)
+    try {
+      const { document } = request
+      await this.uploadAccountDocumentUseCase.upload(document)
+    } catch (error) {
+      return HttpHelper.serverError(error)
+    }
   }
 }
