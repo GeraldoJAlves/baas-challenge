72410e938136c426abc6bb80188071eda81907b4
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-17 20:58:39 -0300

    test: ensure UploadAccountDocumentUseCase calls uploadAccountDocumentStorage with correct document

---
+++ tests/domain/usecases/upload-account-document-usecase.spec.js
@@ -0,0 +1,38 @@
+const { UploadAccountDocumentUseCase } = require('../../../src/domain/usecases')
+
+const makeSut = () => {
+  const uploadAccountDocumentStorageSpy = makeUploadAccountDocumentStorageSpy()
+  const sut = new UploadAccountDocumentUseCase({
+    uploadAccountDocumentStorage: uploadAccountDocumentStorageSpy
+  })
+  return {
+    sut,
+    uploadAccountDocumentStorageSpy
+  }
+}
+
+const makeUploadAccountDocumentStorageSpy = () => {
+  class UploadAccountDocumentStorage {
+    path = '/s3/bucket/any_file'
+    async upload (document) {
+      this.document = document
+      return this.path
+    }
+  }
+  return new UploadAccountDocumentStorage()
+}
+
+const makeDocument = () => ({
+  name: 'any_document',
+  mimetype: 'application/pdf',
+  mv: () => {}
+})
+
+describe('Class Test', () => {
+  test('Should call uploadAccountDocumentStorage with correct document', async () => {
+    const { sut, uploadAccountDocumentStorageSpy } = makeSut()
+    const document = makeDocument()
+    await sut.upload(document)
+    expect(uploadAccountDocumentStorageSpy.document).toEqual(document)
+  })
+})
