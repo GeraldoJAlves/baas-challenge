4895f2cc8afc67745a07aab67a2fc511fb429b36
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-17 20:40:39 -0300

    feat: ensure SaveAccountDocumentCtrl calls validation with correct input

--- src/presentation/controllers/save-account-document-controller.js
+++ src/presentation/controllers/save-account-document-controller.js
@@ -1,12 +1,14 @@
 const { HttpHelper } = require('../helpers')
 
 module.exports = class SaveAccountDocumentController {
-  constructor ({ uploadAccountDocumentUseCase } = {}) {
+  constructor ({ uploadAccountDocumentUseCase, validation } = {}) {
     this.uploadAccountDocumentUseCase = uploadAccountDocumentUseCase
+    this.validation = validation
   }
 
   async handle (request) {
     try {
+      this.validation.validate(request)
       const { document } = request
       await this.uploadAccountDocumentUseCase.upload(document)
       return HttpHelper.noContent()
