a0c79bd9cad434dcb062df1d67c561307b0e5bba
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-17 16:10:11 -0300

    feat: ensure LoadAccountDetailsUseCase throws if no accountId is provided

--- src/domain/usecases/load-account-details-usecase.js
+++ src/domain/usecases/load-account-details-usecase.js
@@ -1,9 +1,12 @@
+const { MissingParamError } = require('../../presentation/errors')
+
 module.exports = class LoadAccountDetailsUseCase {
   constructor ({ loadAccountDetailsRepository }) {
     this.loadAccountDetailsRepository = loadAccountDetailsRepository
   }
 
   async load (accountId) {
+    if (!accountId) throw new MissingParamError('accountId')
     return await this.loadAccountDetailsRepository.loadDetails(accountId)
   }
 }
