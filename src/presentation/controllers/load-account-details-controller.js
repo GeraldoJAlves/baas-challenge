3119bac7a8fe76110f845b166b84ac47e66130a4
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-17 15:50:07 -0300

    feat: ensure LoadAccountDetailsController calls validation with correct values

--- src/presentation/controllers/load-account-details-controller.js
+++ src/presentation/controllers/load-account-details-controller.js
@@ -2,13 +2,17 @@ const { HttpHelper } = require('../helpers')
 
 module.exports = class LoadAccountDetailsController {
   constructor ({
-    loadAccountDetailsUseCase
+    loadAccountDetailsUseCase,
+    validation
   }) {
     this.loadAccountDetailsUseCase = loadAccountDetailsUseCase
+    this.validation = validation
   }
 
   async handle (request) {
     try {
+      const error = this.validation.validate(request)
+      if (error) return HttpHelper.badRequest(error)
       const { accountId } = request
       const account = await this.loadAccountDetailsUseCase.load(accountId)
       if (!account) return HttpHelper.notFound()
