73d85dabddd39168e2a587efc8327bf1a86cd724
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-18 00:16:00 -0300

    feat: authorize AWS S3

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -2,5 +2,13 @@ module.exports = {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/baas-challenge',
   port: process.env.PORT || 5050,
   salt: 12,
-  jwtSecret: process.env.JWT_SECRET || 'KEt8Ir$!umS+HSX*~!Og4=sE8E?7^*'
+  jwtSecret: process.env.JWT_SECRET || 'KEt8Ir$!umS+HSX*~!Og4=sE8E?7^*',
+  AWSConfigs: {
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID || 'ACCESSKEYAWSUSER',
+      secretAccessKey: process.env.AWS_SECRET_KEY || 'sEcreTKey'
+    },
+    endpoint: process.env.AWS_S3_SERVER || 'http://localhost:4572',
+    bucketName: process.env.AWS_BUCKET_NAME || 'demo-bucket'
+  }
 }
--- src/main/server.js
+++ src/main/server.js
@@ -1,8 +1,14 @@
 const { MongoHelper } = require('../infra/db/mongodb')
+const { AWSS3Helper } = require('../infra/storage/aws-s3')
 const env = require('./config/env')
 
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
+    AWSS3Helper.authorize(
+      env.AWSConfigs.credentials,
+      env.AWSConfigs.endpoint,
+      env.AWSConfigs.bucketName
+    )
     const setupApp = await require('./config/app')
     const app = await setupApp()
     app.listen(env.port, () => console.log('Server running'))
