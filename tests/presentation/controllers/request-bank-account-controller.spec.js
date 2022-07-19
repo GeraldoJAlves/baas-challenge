d0faa724b66e3982c572cc4d034b9ede24c35030
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-18 23:13:19 -0300

    test: ensure RequestBankAccountController returns 409 if the account already requested

--- tests/presentation/controllers/request-bank-account-controller.spec.js
+++ tests/presentation/controllers/request-bank-account-controller.spec.js
@@ -1,4 +1,5 @@
 const { RequestBankAccountController } = require('../../../src/presentation/controllers')
+const { AccountAlreadyRequestedError } = require('../../../src/presentation/errors')
 const { HttpHelper } = require('../../../src/presentation/helpers')
 
 const makeSut = () => {
@@ -14,9 +15,10 @@ const makeSut = () => {
 
 const makeRequestBankAccountUseCaseSpy = () => {
   class RequestBankAccountUseCase {
+    alreadyRequested = false
     async request (accountId) {
       this.accountId = accountId
-      return this.isValid
+      return this.alreadyRequested
     }
   }
   return new RequestBankAccountUseCase()
@@ -39,5 +41,12 @@ describe('Request Bank Account Controller', () => {
     requestBankAccountUseCaseSpy.request = () => { throw new Error() }
     const response = await sut.handle(makeRequest())
     expect(response).toEqual(HttpHelper.serverError())
+  })
+
+  test('Should return 409 if the account already requested', async () => {
+    const { sut, requestBankAccountUseCaseSpy } = makeSut()
+    requestBankAccountUseCaseSpy.alreadyRequested = true
+    const response = await sut.handle(makeRequest())
+    expect(response).toEqual(HttpHelper.conflict(new AccountAlreadyRequestedError()))
   })
 })
