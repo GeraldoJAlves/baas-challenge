40d47d52d494da8cb9a9ed301922846b9946c60e
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-18 23:00:13 -0300

    feat: ensure RequestBankAccountController returns 500 if requestBankAccountUseCase throws

--- src/presentation/controllers/request-bank-account-controller.js
+++ src/presentation/controllers/request-bank-account-controller.js
@@ -1,3 +1,5 @@
+const { HttpHelper } = require('../helpers')
+
 module.exports = class RequestBankAccountController {
   constructor ({
     requestBankAccountUseCase
@@ -6,6 +8,10 @@ module.exports = class RequestBankAccountController {
   }
 
   async handle ({ accountId } = {}) {
-    await this.requestBankAccountUseCase.request(accountId)
+    try {
+      await this.requestBankAccountUseCase.request(accountId)
+    } catch (error) {
+      return HttpHelper.serverError(error)
+    }
   }
 }
