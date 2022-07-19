94e9df832f70f03f87a96d6c9c6db3722ea091a7
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-18 22:03:36 -0300

    feat: create /api/account/document route

--- src/main/routes/account-routes.js
+++ src/main/routes/account-routes.js
@@ -1,8 +1,13 @@
 const { expressRouteAdapter } = require('../adapters')
-const { makeSaveAccountDetailsController, makeLoadAccountDetailsController } = require('../factories')
+const {
+  makeSaveAccountDetailsController,
+  makeLoadAccountDetailsController,
+  makeSaveAccountDocumentController
+} = require('../factories')
 const { userAuth } = require('../midlewares')
 
 module.exports = (router) => {
   router.post('/account/details', userAuth, expressRouteAdapter(makeSaveAccountDetailsController()))
   router.get('/account/details', userAuth, expressRouteAdapter(makeLoadAccountDetailsController()))
+  router.post('/account/document', userAuth, expressRouteAdapter(makeSaveAccountDocumentController()))
 }
