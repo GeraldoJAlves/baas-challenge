5ed0fa7b5e827998bc81ca04e04d3ee46a748bb9
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-18 22:31:25 -0300

    test: ensure StorageAdapter throw if file upload fails

--- tests/infra/storage/aws-s3/storage-adapter.spec.js
+++ tests/infra/storage/aws-s3/storage-adapter.spec.js
@@ -15,16 +15,53 @@ const makeSut = () => {
 const makeFile = () => ({ name: 'any_filename', mimetype: 'text/plain', data: 'any_data_document' })
 
 describe('Storage Adapter', () => {
-  test('Should call AWS S3 with correct values', async () => {
-    const { sut } = makeSut()
-    const response = await sut.upload({
-      file: makeFile,
-      key: 'any_file.txt'
+  describe('upload()', () => {
+    test('Should call AWS S3 with correct values', async () => {
+      const { sut } = makeSut()
+      const filePath = await sut.upload({
+        file: makeFile(),
+        key: 'any_file.txt'
+      })
+      expect(filePath).toEqual({
+        bucket: 'any_bucket',
+        key: 'any_file.txt'
+      })
+      expect(s3Mock).toHaveReceivedCommandTimes(PutObjectCommand, 1)
     })
-    expect(response).toEqual({
-      bucket: 'any_bucket',
-      key: 'any_file.txt'
+
+    test('Should throw if file upload fails', async () => {
+      const { sut } = makeSut()
+      s3Mock.on(PutObjectCommand).rejects()
+      const promise = sut.upload({
+        file: makeFile(),
+        key: 'any_file.txt'
+      })
+      expect(promise).rejects.toThrow()
     })
-    expect(s3Mock).toHaveReceivedCommandTimes(PutObjectCommand, 1)
+
+    test('Should throw if no file is provided', async () => {
+      const { sut } = makeSut()
+      const promise = sut.upload({
+        key: 'any_file.txt'
+      })
+      expect(promise).rejects.toThrow()
+    })
+
+    test('Should throw if no key is provided', async () => {
+      const { sut } = makeSut()
+      const promise = sut.upload({
+        file: makeFile()
+      })
+      expect(promise).rejects.toThrow()
+    })
+
+    test('Should throw if no an invalid file is provided', async () => {
+      const { sut } = makeSut()
+      const promise = sut.upload({
+        file: 'invalid_file',
+        key: 'any_file.txt'
+      })
+      expect(promise).rejects.toThrow()
+    })
   })
 })
