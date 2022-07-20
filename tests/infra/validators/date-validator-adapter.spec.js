8eed40cd9a0c629f5135eb77d0d52c3429abf576
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-19 23:20:45 -0300

    test: ensure DateValidatorAdapter returns true if Validator returns true

--- tests/infra/validators/date-validator-adapter.spec.js
+++ tests/infra/validators/date-validator-adapter.spec.js
@@ -14,6 +14,19 @@ describe('Date Validator Adapter', () => {
     expect(validator.options.format).toBe('YYYY-MM-DD')
   })
 
+  test('Should return true if Validator returns true', () => {
+    const { sut } = makeSut()
+    const isValid = sut.isValid('valid_date')
+    expect(isValid).toBeTruthy()
+  })
+
+  test('Should return false if Validator returns false', () => {
+    const { sut } = makeSut()
+    validator.isDateValid = false
+    const isValid = sut.isValid('invalid_date')
+    expect(isValid).toBeFalsy()
+  })
+
   test('Should throw if no date is provided', () => {
     const { sut } = makeSut()
     expect(() => { sut.isValid() }).toThrow()
