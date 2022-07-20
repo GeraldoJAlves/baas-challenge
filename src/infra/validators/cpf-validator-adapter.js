24705cd7c1c09310dee0c106405ce06f41cc5561
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-19 21:41:08 -0300

    feat: ensure CpfValidator throws if no cpf is provided

--- src/infra/validators/cpf-validator-adapter.js
+++ src/infra/validators/cpf-validator-adapter.js
@@ -1,7 +1,9 @@
 const { cpf } = require('cpf-cnpj-validator')
+const { MissingParamError } = require('../../presentation/errors')
 
 module.exports = class CpfValidatorAdapter {
   isValid (cpfNumber) {
+    if (!cpfNumber) throw new MissingParamError('cpfNumber')
     return cpf.isValid(cpfNumber)
   }
 }
