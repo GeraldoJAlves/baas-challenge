bafe3b843190cb417289a829ea176d8255b28fdf
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-19 22:10:17 -0300

    feat: ensure /api/account/details check cpf number

--- src/main/factories/controllers/save-account-details-validation-factory.js
+++ src/main/factories/controllers/save-account-details-validation-factory.js
@@ -1,10 +1,16 @@
-const { RequiredFieldValidation, ValidationComposite } = require('../../../validation/validators')
+const { CpfValidatorAdapter } = require('../../../infra/validators')
+const { RequiredFieldValidation, ValidationComposite, CpfValidation } = require('../../../validation/validators')
 
 module.exports = () => {
   const validations = []
-  for (const field of ['fullName', 'birthDate', 'motherName', 'fatherName', 'cpf', 'rg']) {
-    validations.push(new RequiredFieldValidation(field))
+  for (const fieldName of ['fullName', 'birthDate', 'motherName', 'fatherName', 'cpf', 'rg']) {
+    validations.push(new RequiredFieldValidation(fieldName))
   }
+
+  validations.push(new CpfValidation({
+    fieldName: 'cpf',
+    cpfValidator: new CpfValidatorAdapter()
+  }))
 
   return new ValidationComposite(validations)
 }
