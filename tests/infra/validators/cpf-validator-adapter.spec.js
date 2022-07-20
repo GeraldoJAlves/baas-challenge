ba9dd6ad5bd2862b083812483c2595fd5506294b
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-19 21:37:12 -0300

    test: ensure CpfValidator returns same response from cpf-cnpj-validator

--- tests/infra/validators/cpf-validator-adapter.spec.js
+++ tests/infra/validators/cpf-validator-adapter.spec.js
@@ -8,10 +8,29 @@ const makeSut = () => {
 }
 
 describe('Cpf Validator', () => {
+  beforeEach(() => {
+    cpf.validNumber = true
+  })
+
   test('Should call cpf-cnpj-validator with correct value', async () => {
     const { sut } = makeSut()
     const cpfNumber = '164849165'
     await sut.isValid(cpfNumber)
     expect(cpf.value).toBe(cpfNumber)
+  })
+
+  test('Should return true if cpf-cnpj-validator returns true', async () => {
+    const { sut } = makeSut()
+    const cpfNumber = '164849165'
+    const isValid = await sut.isValid(cpfNumber)
+    expect(isValid).toBeTruthy()
+  })
+
+  test('Should return false if cpf-cnpj-validator returns false', async () => {
+    const { sut } = makeSut()
+    const cpfNumber = '164849165'
+    cpf.validNumber = false
+    const isValid = await sut.isValid(cpfNumber)
+    expect(isValid).toBeFalsy()
   })
 })
