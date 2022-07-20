4c8aa4442ad556531c2143bb8ecb85cf74f4fd17
Geraldo Alves <junior.alves.2007@gmail.com>
2022-07-19 21:41:32 -0300

    test: ensure CpfValidator throws if no cpf is provided

--- tests/infra/validators/cpf-validator-adapter.spec.js
+++ tests/infra/validators/cpf-validator-adapter.spec.js
@@ -12,25 +12,37 @@ describe('Cpf Validator', () => {
     cpf.validNumber = true
   })
 
-  test('Should call cpf-cnpj-validator with correct value', async () => {
+  test('Should call cpf-cnpj-validator with correct value', () => {
     const { sut } = makeSut()
     const cpfNumber = '164849165'
-    await sut.isValid(cpfNumber)
+    sut.isValid(cpfNumber)
     expect(cpf.value).toBe(cpfNumber)
   })
 
-  test('Should return true if cpf-cnpj-validator returns true', async () => {
+  test('Should return true if cpf-cnpj-validator returns true', () => {
     const { sut } = makeSut()
     const cpfNumber = '164849165'
-    const isValid = await sut.isValid(cpfNumber)
+    const isValid = sut.isValid(cpfNumber)
     expect(isValid).toBeTruthy()
   })
 
-  test('Should return false if cpf-cnpj-validator returns false', async () => {
+  test('Should return false if cpf-cnpj-validator returns false', () => {
     const { sut } = makeSut()
     const cpfNumber = '164849165'
     cpf.validNumber = false
-    const isValid = await sut.isValid(cpfNumber)
+    const isValid = sut.isValid(cpfNumber)
     expect(isValid).toBeFalsy()
+  })
+
+  test('Should throw if no cpf is provided', () => {
+    const { sut } = makeSut()
+    expect(() => { sut.isValid() }).toThrow()
+  })
+
+  test('Should throw if cpf-cnpj-validator throws', () => {
+    const { sut } = makeSut()
+    const cpfNumber = '164849165'
+    cpf.isValid = () => { throw new Error() }
+    expect(() => { sut.isValid(cpfNumber) }).toThrow()
   })
 })
